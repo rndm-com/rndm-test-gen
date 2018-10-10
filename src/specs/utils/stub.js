8c56ff1b1808643ca39a7d0ebd2820671464dd4e
rndm-com <rndm.dapps@gmail.com>
2018-10-11 09:57:00 +1100

    Version 0.1.6: Proxyquire falls back on noCallThru

--- src/specs/utils/stub.js
+++ src/specs/utils/stub.js
@@ -4,7 +4,14 @@ import { src } from '../../utils/paths';
 const stub = ({ from, stubs = {}, returnDefault = true } = {}) => {
   if (!from) return;
   const file = src + from;
-  const result = proxyquire(file, stubs);
+  let result;
+  try {
+    proxyquire.callThru()
+    result = proxyquire(file, stubs);
+  } catch (_) {
+    proxyquire.noCallThru()
+    result = proxyquire(file, stubs);
+  }
   return returnDefault ? (result.default || result) : result;
 };
 
