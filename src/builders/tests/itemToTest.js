db46176616e522d5facd6df69a71b2a9daef969e
rndm-com <rndm.dapps@gmail.com>
2019-02-06 16:33:33 +1100

    Version 0.2.8: Pass empty array or array with empty object as arg function.

--- src/builders/tests/itemToTest.js
+++ src/builders/tests/itemToTest.js
@@ -4,7 +4,7 @@ import TYPES from '../../parser/TYPES';
 
 const itemToTest = ({ type, args, returnDefault } = {}) => {
   incrementStat('tests');
-  if ([TYPES.FUNCTION, TYPES.CLASS, TYPES.REACT_CLASS, TYPES.STATIC].includes(type)) return testBuilder({ args: typeof args === 'object' ? args : [type === TYPES.REACT_CLASS ? {} : undefined], returnDefault });
+  if ([TYPES.FUNCTION, TYPES.CLASS, TYPES.REACT_CLASS, TYPES.STATIC].includes(type)) return testBuilder({ args: typeof args === 'object' ? args : type === TYPES.REACT_CLASS ? [{}] : [], returnDefault });
   return testBuilder({ returnDefault });
 };
 export default itemToTest;
