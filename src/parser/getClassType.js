7baa06cce21a56be382d158cd5bc7ca6863e4ae2
rndm-com <rndm.dapps@gmail.com>
2019-02-06 15:30:18 +1100

    Version 0.2.7: Fix missing import

--- src/parser/getClassType.js
+++ src/parser/getClassType.js
@@ -1,2 +1,4 @@
+import TYPES from './TYPES';
+
 const getClassType = (match = '') => match.includes('extends') && match.includes('Component') ? TYPES.REACT_CLASS : TYPES.CLASS;
 export default getClassType;
